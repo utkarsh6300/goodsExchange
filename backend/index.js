ef349e286462dc36433b7b23a2b7fa86b8327af5
utkarsh yadav <utkarshyadav9876@gmail.com>
2024-05-23 12:36:06 +0000

    added export

--- backend/index.js
+++ backend/index.js
@@ -33,13 +33,10 @@ app.use('/api/user',require('./routes/api/user'));
 
 
 
-
-
-
-
-
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+module.exports = app;
