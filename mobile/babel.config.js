72388ae3e82df56b4bdc338b185e2b553c6cc6f3
Utkarsh Yadav <utkarshyadav9876@gmail.com>
2026-04-25 18:39:40 +0530

    fixed app crash.

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -3,10 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "expo-router/babel",
-      ["@babel/plugin-transform-class-properties", { "loose": true }],
-      ["@babel/plugin-transform-private-methods", { "loose": true }],
-      ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
       ["module:react-native-dotenv", {
         "moduleName": "@env",
         "path": ".env",
